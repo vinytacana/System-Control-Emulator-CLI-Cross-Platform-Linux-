d68f4d6eb90a65f5d518af1bd9902f20a49c535c
Vinny Tacaná <vini.tacana@gmail.com>
2026-02-03 00:24:03 -0400

    feat: auxiliary function to execute a command and retrieve the output as a string

--- emulador.cpp
+++ emulador.cpp
@@ -765,3 +765,18 @@ void definir_estado_bt(bool ligar)
         perror("fork falhou");
     }
 }
+
+std::string exec_command(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() falhou!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+
